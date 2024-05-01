b4f456b4bcdc02d655d323dfe8dabe7c53fde466
MatiBravo47 <matibravoneron@gmail.com>
2024-05-01 09:11:15 -0300

    add guia 3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            1)Edad para conducir:
+            a)Pregunta: Ingresa tu edad para determinar si puedes obtener 
+            tu licencia de conducir.
+            b)Variables: int (edad)
+            c)Condicionales: if-else
+            */
+            int edad;
+            string resultado;
+            Console.WriteLine("Ingresa tu edad");
+            edad = int.Parse(Console.ReadLine());
+            resultado = edad > 18 ? "Puede conducir" : "No puede conducir";
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+
+            /*
+             * 
+             * 
+            */ 
+        }
+    }
+}
